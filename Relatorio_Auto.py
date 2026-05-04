89c4bf7453fc2f45a82507bab5dbffe95c6506e5
Iago Mazzoni Santos <iago.santos@caed.local>
2026-05-04 15:41:07 -0300

    inserindo um menu iterativo simples pelo terminal

--- Relatorio_Auto.py
+++ Relatorio_Auto.py
@@ -1,6 +1,8 @@
 import xlsxwriter
 import pyodbc
 
+
+
 def pedeSenha():
     usuario = input("Digite seu usuário: ")
     senha = input("Digite sua senha: ")
@@ -30,15 +32,14 @@ def criaExcel(nome_arquivo):
     workbook = xlsxwriter.Workbook(nome_arquivo)
     return workbook
 
-def main():
-    print("Programa iniciado")
+def criarRelatorio():
+    ##aqui a ideia é chamar as funções para criar o relatório, como se fosse uma main
 
     conectado = False
     conexao = None
 
     while conectado is not True:
         conectado, conexao = pedeSenha()
-
     
 
 
@@ -47,6 +48,67 @@ def main():
     
     conexao.close()
     workbook.close()
+
+    return
+
+
+def main():
+    
+    menu()
+
+
+
+def menu():
+    while True:
+        print("===== MENU PRINCIPAL =====")
+        print("1 - Criar relatório")
+        print("2 - Sair")
+
+        opcao = input("Escolha uma opção: ")
+
+        if opcao == "1":
+
+            while True:
+                print("Selecione a hierarquia do projeto:")
+                print("1 - Estado, Regional, Municipio, Escola, Turma")
+                print("2 - Municipio, Escola, Turma")
+                print("3 - Municipio, Regional, Escola, Turma")
+                print("4 - Inserir outra hierarquia")
+                print("5 - Sair")
+                opcaoHierarquia = input("Escolha uma opção: ")
+
+                if opcaoHierarquia == "1":
+                    criarRelatorio("Estado,Regional,Municipio,Escola,Turma")
+                    break
+
+                elif opcaoHierarquia == "2":
+                    criarRelatorio("Municipio,Escola,Turma")
+                    break
+
+                elif opcaoHierarquia == "3":
+                    criarRelatorio("Municipio,Regional,Escola,Turma")
+                    break
+
+                elif opcaoHierarquia == "4":
+                    hierarquia = input("Escreva a hierarquia, separando por virgula e sem espaços. (Ex: Municipio,Escola,Turma)")
+                    criarRelatorio(hierarquia)
+                    break
+
+                elif opcaoHierarquia == "5":
+                    print("Saindo")
+                    break
+
+                else:
+                    print("Opção inválida. Tente novamente.\n")
+
+            
+
+        elif opcao == "2":
+            print("Encerrando programa...")
+            break
+
+        else:
+            print("Opção inválida. Tente novamente.\n")
 
 if __name__ == "__main__":
     main()
